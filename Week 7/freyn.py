84c220617be86ce87a79eeca635c7b18a0f8b9ae
ninjafiveo <mjsekol@live.com>
2025-10-14 09:51:14 -0400

    Python Classes

---
+++ Week 7/freyn.py
@@ -0,0 +1,37 @@
+
+classroom_students = ["Freyn", "Adelman", "Sekol", "Simms"]
+
+freyn = {
+    "name": "Lucas",
+    "grade": 92,
+    "lab": "Software",
+}
+
+adelman = {
+    "name": "Marcus",
+    "grade": 94,
+    "lab": "Cosmetology",
+}
+sekol = {
+    "name": "Michael",
+    "grade": 67,
+    "lab": "Welding"
+}
+
+simms = {
+    "name": "Jayden",
+    "grade": 91,
+    "lab": "Software"
+}
+
+students = {
+    "Freyn": freyn,
+    "Adelman": adelman,
+    "Sekol": sekol,
+    "Simms": simms,
+}
+
+for student_key in classroom_students:
+    student = students[student_key]
+    print(f"Student name: {student['name']}, Grade: {student['grade']}, Lab: {student['lab']}")
+
